19a15c0596e7e6645f3e6f1b6e7c3f7d8b563bb7
Kyle44 <kyle44@comcast.net>
2015-04-02 01:05:43 -0400

    Create Shape.cpp

---
+++ Shape.cpp
@@ -0,0 +1,16 @@
+// File: Shape.cpp
+
+#include "Shape.h"
+#include <iostream>
+using namespace std;
+
+Shape::~Shape()
+{
+// no code
+}
+
+void Shape::Draw() const
+{
+  cout << "Drawing Shape..." << endl;
+// Write code here such that we can identify Shape's Draw is invoked
+}
